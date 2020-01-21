bf4a48c74dd1858579f410de92018cba906d8c46
Grzegorz Sieczkowski <grzegorz.sieczkowski@10clouds.com>
2020-01-21 10:31:59 +0100

    background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,36 @@
 import React from "react"
 
+import { graphql } from "gatsby";
+
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import BackgroundSection from '../components/globals/backgroundSection';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    INDEX
-  </Layout>
-)
+export const query = graphql`
+query{
+  bg:file(relativePath:{eq:"default-background.jpeg"}){
+    childImageSharp{
+      fluid{
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`;
+
+
+const IndexPage = ({data}) => {
+  console.log( data );
+
+  return (
+    <Layout>
+
+      <SEO title="Home" />
+
+      <BackgroundSection image={data.bg.childImageSharp} title="Coffee shop" styleClass="default-background"/>
+
+    </Layout>
+  )
+}
 
 export default IndexPage
